e3c67fb270a03b324633fa0ff66a3440161930b5
David Neal <davidneal@Davids-iMac.local>
2017-08-26 11:00:01 -0400

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -10,6 +10,9 @@ var {
 var {
   User
 } = require('./models/user');
+var {
+  ObjectID
+} = require('mongodb');
 
 var app = express();
 
@@ -24,7 +27,7 @@ app.post('/todos', (req, res) => {
     res.send(doc);
   }, (e) => {
     res.status(400).send(e);
-  })
+  });
 });
 
 app.get('/todos', (req, res) => {
@@ -34,8 +37,37 @@ app.get('/todos', (req, res) => {
     });
   }, (e) => {
     res.status(400).send(e);
-  })
-})
+  });
+});
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({
+      todo
+    });
+  }).catch((e) => {
+    res.status(400).send()
+  });
+});
+
+//   Todo.find().then((todos) => {
+//     res.send({
+//       todos
+//     });
+//   }, (e) => {
+//     res.status(400).send(e);
+//   })
+// })
 
 app.listen(3000, () => {
   console.log('Server listening on port 3000.');
